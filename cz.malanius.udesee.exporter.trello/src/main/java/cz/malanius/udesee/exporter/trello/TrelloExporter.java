8cb5da9ba312aa798d67d85cd07d1113e1cc706e
Malanius <malanius@seznam.cz>
2018-07-02 14:02:29 +0200

    Implement export to Trello via it's REST API

--- cz.malanius.udesee.exporter.trello/src/main/java/cz/malanius/udesee/exporter/trello/TrelloExporter.java
+++ cz.malanius.udesee.exporter.trello/src/main/java/cz/malanius/udesee/exporter/trello/TrelloExporter.java
@@ -1,11 +1,84 @@
 package cz.malanius.udesee.exporter.trello;
 
+import cz.malanius.udesee.cli.util.ConsoleReader;
+import cz.malanius.udesee.cli.util.ConsoleWriter;
+import cz.malanius.udesee.course.Course;
+import cz.malanius.udesee.course.Lesson;
+import cz.malanius.udesee.course.Section;
 import cz.malanius.udesee.exporter.Exporter;
+import cz.malanius.udesee.exporter.trello.api.Requester;
+import cz.malanius.udesee.exporter.trello.pojo.board.Board;
+import cz.malanius.udesee.exporter.trello.pojo.card.Card;
+import cz.malanius.udesee.exporter.trello.pojo.checklist.Checklist;
+import cz.malanius.udesee.exporter.trello.pojo.list.TList;
+import cz.malanius.udesee.exporter.trello.pojo.member.Member;
+import cz.malanius.udesee.exporter.trello.pojo.token.Token;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class TrelloExporter implements Exporter {
-    @Override
-    public void exportWithCli() {
 
+    private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(TrelloExporter.class);
+
+    private Requester requester = new Requester();
+
+    @Override
     public void exportWithCli(Course course) {
+        LOG.trace("exportWithCli() - start.");
+
+        ConsoleWriter.printUserInfo("Trello export module started.");
+        try {
+
+            //API key
+            ConsoleWriter.printUserInfo("Please provide your Trello API key (you can get one at https://trello.com/app-key): ");
+            requester.setKey(ConsoleReader.readLine());
+
+            //Token
+            ConsoleWriter.printUserInfo("Please get requester token from " + requester.getAuthUrl());
+            ConsoleWriter.printUserInfo("Enter the token:");
+            requester.setToken(ConsoleReader.readLine());
+            Token token = requester.getToken();
+            Member trelloMember = requester.getMember(token.getIdMember());
+            ConsoleWriter.printUserInfo("Successfully connected to Trello as " + trelloMember.getFullName());
+
+            //Board selection
+            List<Board> boards = requester.getMemberBoards(trelloMember.getId());
+            ConsoleWriter.printListSelection(boards);
+            int boardChoice = ConsoleReader.select(boards.size()) - 1;
+            Board selectedBoard = boards.get(boardChoice);
+            ConsoleWriter.printUserInfo("Selected board: " + selectedBoard.getName());
+
+            //List selection
+            List<TList> boardLists = requester.getBoardLists(selectedBoard.getId());
+            ConsoleWriter.printListSelection(boardLists);
+            int listChoice = ConsoleReader.select(boards.size()) - 1;
+            TList selectedList = boardLists.get(listChoice);
+            ConsoleWriter.printUserInfo("Selected list: " + selectedList.getName());
+
+            //Card selection
+            List<Card> cards = requester.getCardsForList(selectedList.getId());
+            ConsoleWriter.printListSelection(cards);
+            int cardChoice = ConsoleReader.select(cards.size()) - 1;
+            Card selectedCard = cards.get(cardChoice);
+            ConsoleWriter.printUserInfo("Selected card: " + selectedCard.getName());
+
+            postChecklists(course, selectedCard.getId());
+
+            ConsoleWriter.printUserOk("Export to Trello was successful!");
+
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void postChecklists(Course course, String cardId) throws IOException, InterruptedException {
+        for (Section section : course.getSections()) {
+            Checklist checklist = requester.postChecklist(section.getSectionLine(), cardId);
+            for (Lesson lesson : section.getLessons()) {
+                requester.postChecklistItem(lesson.getLessonLine(), checklist.getId());
+            }
+        }
     }
 }
---
+++ cz.malanius.udesee.exporter.trello/src/main/java/cz/malanius/udesee/exporter/trello/api/Requester.java
@@ -0,0 +1,174 @@
+package cz.malanius.udesee.exporter.trello.api;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import cz.malanius.udesee.cli.util.ConsoleWriter;
+import cz.malanius.udesee.exporter.trello.pojo.board.Board;
+import cz.malanius.udesee.exporter.trello.pojo.card.Card;
+import cz.malanius.udesee.exporter.trello.pojo.checklist.CheckItem;
+import cz.malanius.udesee.exporter.trello.pojo.checklist.Checklist;
+import cz.malanius.udesee.exporter.trello.pojo.list.TList;
+import cz.malanius.udesee.exporter.trello.pojo.member.Member;
+import cz.malanius.udesee.exporter.trello.pojo.token.Token;
+import cz.malanius.udesee.exporter.trello.webclient.WebClient;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Requester {
+
+    private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(Requester.class);
+
+    private static Gson gson = new Gson();
+
+    final String authPoint = "https://trello.com/1/authorize";
+    final String callback_method = "postMessage";
+    final String return_url = "origin";
+    private final String scope = "read,write";
+    private final String expiration = "1hour";
+    private final String name = "Udesee%20Trello%20Exporter";
+    private final String response_type = "token";
+
+
+    private String key;
+    private String token;
+
+    public URI getAuthUrl() throws URISyntaxException {
+        LOG.trace("getAuthUrl() - start.");
+        StringBuilder sb = new StringBuilder(authPoint);
+
+        sb.append("?")
+                .append("scope=")
+                .append(scope)
+                .append("&name=")
+                .append(name)
+                .append("&expiration=")
+                .append(expiration)
+                .append("&response_type=")
+                .append(response_type)
+                .append("&key=")
+                .append(key);
+
+        URI autUri = new URI(sb.toString());
+        LOG.debug("Generated authorization URL: {}", autUri);
+        return autUri;
+    }
+
+    public Token getToken() throws IOException, InterruptedException {
+        LOG.trace("getToken() - start.");
+
+        String tokenEndpoint = "https://api.trello.com/1/tokens/";
+        URI getTokenUri = URI.create(tokenEndpoint + "/" + token + "?key=" + key + "&token=" + token);
+        LOG.debug("Generated token request URL: {}", getTokenUri);
+        ConsoleWriter.printUserInfo("Requesting token info...");
+
+        String tokenJSON = WebClient.getRequest(getTokenUri);
+        LOG.debug("Token successfully obtained: ()", tokenJSON);
+
+        return gson.fromJson(tokenJSON, Token.class);
+    }
+
+    public Member getMember(String memberId) throws IOException, InterruptedException {
+        LOG.trace("getMember() - start.");
+
+        String memberEndpoint = "https://api.trello.com/1/members/";
+        URI getMemberUri = URI.create(memberEndpoint + memberId + "?key=" + key + "&token=" + token);
+        LOG.debug("Generated member request URL: {}", getMemberUri);
+        ConsoleWriter.printUserInfo("Requesting member info...");
+
+        String memberJSON = WebClient.getRequest(getMemberUri);
+        LOG.debug("Member successfully obtained: {}", memberJSON);
+
+        return gson.fromJson(memberJSON, Member.class);
+    }
+
+    public List<Board> getMemberBoards(String memberId) throws IOException, InterruptedException {
+        LOG.trace("getMemberBoards() - start.");
+
+        String memberBoardsEndpoint = "https://api.trello.com/1/members/" + memberId + "/boards";
+        URI getMemberBoardsUri = URI.create(memberBoardsEndpoint + "?filter=open" + "&key=" + key + "&token=" + token);
+        LOG.debug("Generated member request URL: {}", getMemberBoardsUri);
+        ConsoleWriter.printUserInfo("Requesting member boards...");
+
+        String boardsJSON = WebClient.getRequest(getMemberBoardsUri);
+        LOG.debug("Boards successfully obtained: {}: ", boardsJSON);
+
+        Type listType = new TypeToken<ArrayList<Board>>() {
+        }.getType();
+        return gson.fromJson(boardsJSON, listType);
+    }
+
+    public List<TList> getBoardLists(String boardId) throws IOException, InterruptedException {
+        LOG.trace("getBoardLists() - start.");
+
+        String memberBoardsEndpoint = "https://api.trello.com/1/boards/" + boardId + "/lists";
+        URI getBoarListsUri = URI.create(memberBoardsEndpoint + "?filter=open" + "&cards=none" + "&key=" + key + "&token=" + token);
+        LOG.debug("Generated member request URL: {}", getBoarListsUri);
+        ConsoleWriter.printUserInfo("Requesting board lists...");
+
+        String boardListsJSON = WebClient.getRequest(getBoarListsUri);
+        LOG.debug("Board lists successfully obtained: " + boardListsJSON);
+
+        Type listType = new TypeToken<ArrayList<TList>>() {
+        }.getType();
+        return gson.fromJson(boardListsJSON, listType);
+    }
+
+    public List<Card> getCardsForList(String listId) throws IOException, InterruptedException {
+        LOG.trace("getCardsForList() - start.");
+
+        String listCardsEndpoint = "https://api.trello.com/1/lists/" + listId + "/cards";
+        URI getListCards = URI.create(listCardsEndpoint + "?key=" + key + "&token=" + token);
+        LOG.debug("Generated member request URL: {}", getListCards);
+        ConsoleWriter.printUserInfo("Requesting list cards...");
+
+        String cardsJSON = WebClient.getRequest(getListCards);
+        LOG.debug("Cards successfully obtained: " + cardsJSON);
+
+        Type listType = new TypeToken<ArrayList<Card>>() {
+        }.getType();
+        return gson.fromJson(cardsJSON, listType);
+    }
+
+    public Checklist postChecklist(String checklistName, String cardId) throws IOException, InterruptedException {
+        LOG.trace("postChecklist({}, {}) - start.", checklistName, cardId);
+
+        String postChecklistEndpoint = "https://api.trello.com/1/checklists" + "?idCard=" + cardId + "&name=" + URLEncoder.encode(checklistName, "UTF-8") + "&pos=bottom" + "&key=" + key + "&token=" + token;
+        URI postChecklistUri = URI.create(postChecklistEndpoint);
+        LOG.debug("Generated checklist post request: {}", postChecklistUri);
+
+        ConsoleWriter.printUserInfo("Sending new checklist for section {}..." + checklistName);
+        String checklistJSON = WebClient.postRequest(postChecklistUri);
+        LOG.debug("POST checklist reply: " + checklistJSON);
+
+        return gson.fromJson(checklistJSON, Checklist.class);
+    }
+
+    public CheckItem postChecklistItem(String itemName, String checklistId) throws IOException, InterruptedException {
+        LOG.trace("postChecklistItem({}, {}) - start.", itemName, checklistId);
+
+        String postChekItemEndpoint = "https://api.trello.com/1/checklists/" + checklistId + "/checkItems" + "?name=" + URLEncoder.encode(itemName, "UTF-8") + "&pos=bottom" + "&key=" + key + "&token=" + token;
+        URI postCheckItemUri = URI.create(postChekItemEndpoint);
+        LOG.debug("Generated checklist post item request: {}", postCheckItemUri);
+
+        System.out.println("\tSending checklist item: " + itemName);
+        String chekItemJSON = WebClient.postRequest(postCheckItemUri);
+        LOG.debug("POST check item reply: " + chekItemJSON);
+
+        return gson.fromJson(chekItemJSON, CheckItem.class);
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
