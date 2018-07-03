bc0ef64e84ea660cda1c6fb09620e4009527975c
Malanius <malanius@seznam.cz>
2018-07-03 11:59:56 +0200

    Create Y/N choice in ConsoleReader

--- cz.malanius.udesee.cli/src/main/java/cz/malanius/udesee/cli/util/ConsoleReader.java
+++ cz.malanius.udesee.cli/src/main/java/cz/malanius/udesee/cli/util/ConsoleReader.java
@@ -2,6 +2,9 @@ package cz.malanius.udesee.cli.util;
 
 import java.util.Scanner;
 
+import static cz.malanius.udesee.cli.util.ConsoleWriter.printUserInfo;
+import static cz.malanius.udesee.cli.util.ConsoleWriter.printUserWarn;
+
 public class ConsoleReader {
     private static Scanner scanner = new Scanner(System.in);
 
@@ -18,13 +21,35 @@ public class ConsoleReader {
                 if (chosen > 0 && chosen <= optionsCount) {
                     correct = true;
                 } else {
-                    ConsoleWriter.printUserWarn("Incorrect choice, please choose between 1-" + optionsCount);
+                    printUserWarn("Incorrect choice, please choose between 1-" + optionsCount);
                 }
             } catch (NumberFormatException ex) {
-                ConsoleWriter.printUserWarn("Not a number!");
+                printUserWarn("Not a number!");
             }
 
         }
         return chosen;
+    }
+
+    public static boolean yesOrNo(String prompt, boolean defaultIsYes) {
+        boolean correct = false;
+        String choice = defaultIsYes ? " [Y/n]?" : " [y/N]?";
+        String chosen = null;
+        while (!correct) {
+            printUserInfo(prompt + choice);
+            chosen = scanner.nextLine().toLowerCase();
+            if (!chosen.equals("y") && !chosen.equals("n") && !chosen.equals("")) {
+                printUserWarn("Incorrect choice, please try again!");
+            } else {
+                correct = true;
+            }
+        }
+
+        if (chosen.equals("")) {
+            return defaultIsYes;
+        }
+
+        return chosen.equals("y");
+
     }
 }
